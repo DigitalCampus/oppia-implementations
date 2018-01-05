89d2869ef14ebc41ba81bca8ec27517d1d5cb31d
Alex Little <alex@digital-campus.org>
2018-01-05 17:15:15 +0200

    update

--- oppia_implementations/urls.py
+++ oppia_implementations/urls.py
@@ -11,7 +11,5 @@ v1_api.register(OppiaImplementationResource())
 
 urlpatterns = [
     url(r'^$', oppia_imp_views.home_view, name="oppia_implementations_home"),
-    
-    url(r'^api/', include(v1_api.urls)),
-    
+    url(r'^api/', include(v1_api.urls))
     ]
