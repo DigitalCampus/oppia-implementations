ba8141baa82c025f9dc423c72c84a93bca357e52
Alex Little <alex@digital-campus.org>
2018-01-08 14:10:09 +0200

    update ordering

--- oppia_implementations/views.py
+++ oppia_implementations/views.py
@@ -5,6 +5,6 @@ from django.utils.translation import ugettext_lazy as _
 from oppia_implementations.models import OppiaImplementation, ImplementationDataKV
 
 def home_view(request):
-    implementations = OppiaImplementation.objects.filter(is_visible=True).order_by('-order_by')
+    implementations = OppiaImplementation.objects.filter(is_visible=True).order_by('-order_by', 'title')
     return render(request, 'oppia_implementations/home.html',  
                               {'implementations': implementations})
