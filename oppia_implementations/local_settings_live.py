1b718c07cd484cd8df73e198f9f5baf4a436b75b
Alex Little <alex@digital-campus.org>
2018-12-14 21:38:31 +0000

    update django version and project setup

--- oppia_implementations/local_settings_live.py
+++
@@ -1,8 +0,0 @@
-
-
-def modify(settings):
-    
-    settings['INSTALLED_APPS'] += ('tastypie','sorl.thumbnail',)
-    
-    settings['THUMBNAIL_COLORSPACE'] = None
-    settings['THUMBNAIL_PRESERVE_FORMAT'] = True
