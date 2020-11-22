dfce202e898ed8c9d50cd275ebb6e0624f66a071
alex <consult@alexlittle.net>
2020-11-22 18:38:06 +0200

    update implementations site

--- oppia_implementations/views.py
+++ oppia_implementations/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import render
 from oppia_implementations.models import OppiaImplementation
 
 def home_view(request):
-    implementations = OppiaImplementation.objects.filter(is_visible=True).order_by('-order_by', 'title')
+    implementations = OppiaImplementation.objects.filter(
+        is_visible=True, is_active=True).order_by('-order_by', 'title')
+    prev_imp = OppiaImplementation.objects.filter(
+        is_visible=True, is_active=False).order_by('-order_by', 'title')
     return render(request, 'oppia_implementations/home.html',  
-                              {'implementations': implementations})
+                              {'implementations': implementations,
+                               'previous_implementations': prev_imp})
